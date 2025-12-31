295ac3664861e3a3abf2154f6ec51eab6b48420b
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-12-31 14:50:59 +0100

    update key architecture

--- src/HashFitKeys.sol
+++ src/HashFitKeys.sol
@@ -3,56 +3,110 @@ pragma solidity ^0.8.25;
 import {ERC721A} from "@ERC721A/ERC721A.sol";
 import {IHashFitKey} from "./IHashFitKey.sol";
 
-abstract contract KeyScaffold is ERC721A{
-    address public FACTORY;
-    string public baseURI;
+abstract contract KeyScaffold is ERC721A, IHashFitKey {
+    // How many generations key is valid for
+    uint8 private immutable KEY_VALIDITY;
+    address internal FACTORY;
+    uint64 private immutable GENERATION;
 
-    modifier onlyFactory{
-        if (msg.sender != FACTORY){
+    string public baseURI;
+    
+    constructor(Metadata _keyMetaData, KeyDetail _keydetail) ERC721A(_keyMetadata.name, _keyMetaData.symbol) {
+        FACTORY = msg.sender;
+        _keyMetadata.baseURI = _uri;
+        KEY_VALIDITY = _keydetail.validity;
+        GENERATION = _keydetail.generation;
+    }
+
+    struct Metadata{
+        string name;
+        string symbol;
+        string uri;
+    }
+
+    struct KeyDetail{
+        uint8 validity;
+        uint64 generation;
+    }
+
+    modifier onlyFactory() {
+        if (msg.sender != FACTORY) {
             revert IHashFitKey.UnauthorizedAccess();
         }
         _;
     }
+    
+    /// @dev admin gated function used for manual key distribution.
+    function distributeKeys(Receiver[] memory receivers) external KeyScaffold.onlyFactory {
+        for (uint256 i; i < receivers.length; i++) {
+            _mint(receivers[i].receiverAddress, receivers[i].amount);
+            emit DistributeKeys(receivers[i].receiverAddress, receivers[i].amount);
+        }
+    }
+    
+    /// @dev The drop generation in which key was created.
+    function generation() external returns(uint gen){
+        gen = GENERATION;
+    }
 
-    function _startTokenId() internal pure override returns(uint){
+    /// @dev The time frame for which key usage is valid.
+    /// @notice a validity of 0 means key cannot expire (Mythic tier keys)
+    /// @notice a validity of n (n >= 1) means keys are valid for the next n drops.
+    function keyValidity() external returns(uint validity){
+        validity = KEY_VALIDITY;
+    }
+
+    function _startTokenId() internal pure override returns (uint256) {
         return 1;
     }
 
-    function _baseURI() internal view override returns(string memory){
+    function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
+
+    function burn i
 }
 
-contract HashFitMythic is KeyScaffold, IHashFitKey{
-    uint public immutable generation;
-    
+/// @title HashFit Mythic tier key
+/// Features:
+///       - Non transferrable
+///       - No expiry
+///       - Can be redeemed for an item in a mythic tier (exclusive) apparel drops
+///       - Can be redeemed for exclusive irl perks (future updates)
+contract HashFitMythic is KeyScaffold{
 
-    constructor(string memory name, string memory symbol, string memory _uri) ERC721A(name, symbol){
-        FACTORY = msg.sender;
-        baseURI = _uri;
+    function approve(address to, uint256 tokenId) public payable virtual override {
+        revert MythicKeyNonTransferrable();
     }
-    function distributeKeys(Receiver[] memory receivers) external KeyScaffold.onlyFactory{
-        for (uint i; i < receivers.length; i++){
-            _mint(receivers[i].receiverAddress, receivers[i].amount);
-            emit DistributeKeys(receivers[i].receiverAddress, receivers[i].amount);
-        }
+
+    function setApprovalForAll(address operator, bool approved) public virtual override {
+        revert MythicKeyNonTransferrable();
     }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public payable virtual override {
+        revert MythicKeyNonTransferrable();
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public payable virtual override {
+        revert MythicKeyNonTransferrable();
+    }
+
 }
 
-
-contract HashFitLegendary is KeyScaffold, IHashFitKey{
-    uint public immutable generation;
-
-    constructor(string memory name, string memory symbol, string memory _uri) ERC721A(name, symbol){
-        FACTORY = msg.sender;
-        baseURI = _uri;
-    }
-
-    function distributeKeys(Receiver[] memory receivers) external onlyFactory{
-        for (uint i; i < receivers.length; i++){
-            _mint(receivers[i].receiverAddress, receivers[i].amount);
-            emit DistributeKeys(receivers[i].receiverAddress, receivers[i].amount);
-        }
-    }
+/// @title HashFit Legendary tier key
+/// Features:
+///        - Transferrable
+///        - Can be used to purchase an item in Legendary tier apparel drops or lower
+///        - Expires if not used within the time frame for which its usage is valid
+contract HashFitLegendary is KeyScaffold{
 }
 
