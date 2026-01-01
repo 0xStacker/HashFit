45618b441b67b5d25c6de5859d220d9809aa05c0
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-01-01 18:41:30 +0100

    update, refactor + document

--- src/HashFit.sol
+++ src/HashFit.sol
@@ -5,9 +5,14 @@ import {IHashFitKey} from "./IHashFitKey.sol";
 import {IERC721A} from "@ERC721A/IERC721A.sol";
 import {IHashFitFactory} from "./IHashFitFactory.sol";
 
-contract HashFit is ERC1155{
-    // How many generations key is valid for
-    uint8 immutable KEY_VALIDITY;
+/// @title HashFit Apparel Drop
+/// @author Ibrahim
+/**
+ * A loyalty and identity system for a web3 backed sports wear brand
+ *  
+ */ 
+
+contract HashFit is ERC1155 {
     // Total unit of items in the drop
     uint64 public immutable TOTAL_SUPPLY;
     // Current Drop Generation
@@ -18,23 +23,26 @@ contract HashFit is ERC1155{
     uint256 immutable CYPHERING_PHASE_DURATION;
 
     address private immutable FACTORY;
-  
+
     string public contractUri;
 
-    mapping(uint256  => Item) dropItems;
+    mapping(uint256 => Item) dropItems;
     // Number of items that has been sold in the drop
     uint256 public totalSoldItems;
 
-    //
-    struct HashFitDrop{
-        uint64 totalSupply;
-        uint64 generation;
-        uint256 saleStartTime;
-        uint256 cypheringPhaseDuration;
+    // Holds required data for an apparel drop
+    // Cyphering phase is a special limited sale phase at the begining of the drop
+    // Purchasing an item within this phase would allow buyer wallet to be collected and considered
+    // for random HashFit keys distribution offchain
+    struct HashFitDrop {
+        uint64 totalSupply; // How many individual items are in the drop
+        uint64 generation; // Drop generation
+        uint256 saleStartTime; // When the drop sale begins
+        uint256 cypheringPhaseDuration; // 
     }
 
     // Per item details
-    struct Item{
+    struct Item {
         uint64 maxSupply; // Total units of an item present in the drop
         uint64 currentSupply; // Total number of an item that has been sold
         uint256 price; // Selling price of a unit of an item
@@ -43,62 +51,64 @@ contract HashFit is ERC1155{
     }
 
     // Item purchase details
-    struct SaleItem{
+    struct SaleItem {
         uint8 itemId; // Unique item identifier
         uint64 amount; // Unit of item being purchased
     }
 
     // HashFit key details
-    struct Key{
-        uint64 keyGen; 
+    struct Key {
+        uint64 keyGen;
         uint64 keyId;
     }
 
-    event PurchaseAndClaim(uint item, uint amount, bool key);
-    event RedeemKey(address redeemer, uint keyGen, uint keyId); 
+    event PurchaseAndClaim(uint256 item, uint256 amount, bool key);
+    event RedeemKey(address redeemer, uint256 keyGen, uint256 keyId);
 
     error SaleNotStarted();
-    error NonTransferrable(); 
+    error NonTransferrable();
     error NotEnoughItems();
     error InsufficientFund();
     error UriRequestForNonExistentToken();
     error InsufficientKeys(address);
-    error UnauthorizedKeyUsage(uint, uint);
+    error UnauthorizedKeyUsage(uint256, uint256);
     error UnableToTransferKey();
     error KeyMismatch();
-    error ExpiredKey(uint);
-    error CannotPurchaseItem(uint itemId, uint amount);
+    error ExpiredKey(uint256);
+    error CannotPurchaseItem(uint256 itemId, uint256 amount);
 
-    constructor(string memory _uri, HashFitDrop memory setup
-        ) ERC1155(_uri){
-            TOTAL_SUPPLY = setup.totalSupply;
-            GENERATION = setup.generation;
-            SALE_START_TIME = setup.saleStartTime;
-            CYPHERING_PHASE_DURATION = setup.cypheringPhaseDuration;
+    constructor(string memory _uri, HashFitDrop memory setup) ERC1155(_uri) {
+        TOTAL_SUPPLY = setup.totalSupply;
+        GENERATION = setup.generation;
+        SALE_START_TIME = setup.saleStartTime;
+        CYPHERING_PHASE_DURATION = setup.cypheringPhaseDuration;
     }
-    
+
     /// @dev Purchase n units of m items
-    function purchaseAndClaim(SaleItem[] memory _items) external payable{
-        if (block.timestamp < SALE_START_TIME){
+    function purchaseAndClaim(SaleItem[] memory _items) external payable {
+        if (block.timestamp < SALE_START_TIME) {
             revert SaleNotStarted();
         }
-        if (totalSoldItems + _items.length > TOTAL_SUPPLY){
+        if (totalSoldItems + _items.length > TOTAL_SUPPLY) {
             revert NotEnoughItems();
         }
 
-        uint totalCost;
-        //Calculate cost
-        for (uint8 i; i < _items.length; i++){
+        uint256 totalCost;
+        // Handle sale of all items requested.
+        for (uint8 i; i < _items.length; i++) {
             SaleItem memory currentItem = _items[i];
 
-            if (dropItems[currentItem.itemId].currentSupply + currentItem.amount > dropItems[currentItem.itemId].maxSupply){
+            if (
+                dropItems[currentItem.itemId].currentSupply + currentItem.amount
+                    > dropItems[currentItem.itemId].maxSupply
+            ) {
                 revert CannotPurchaseItem(currentItem.itemId, currentItem.amount);
             }
             totalCost += dropItems[currentItem.itemId].price * currentItem.amount;
-            if (msg.value < totalCost){
+            if (msg.value < totalCost) {
                 revert InsufficientFund();
-            } 
-           
+            }
+
             dropItems[currentItem.itemId].currentSupply += currentItem.amount;
             totalSoldItems += currentItem.amount;
             emit PurchaseAndClaim(currentItem.itemId, currentItem.amount, false);
@@ -106,32 +116,46 @@ contract HashFit is ERC1155{
         }
     }
 
-    function purchaseWithKey(SaleItem[] memory _items, Key[] memory keys) external{
-        if (_items.length != keys.length){
+    /// @dev Purchase items from drop using key in a 1:1 format
+    function purchaseWithKey(SaleItem[] memory _items, Key[] memory keys) external {
+        // Sanity check
+        if (_items.length != keys.length) {
             revert KeyMismatch();
         }
-        for(uint i; i < _items.length; i++){
+        // Ensure sale has begun
+        if(block.timestamp < SALE_START_TIME){
+            revert SaleNotStarted();
+        }
+        for (uint256 i; i < _items.length; i++) {
             SaleItem memory currentItem = _items[i];
-            if (dropItems[currentItem.itemId].currentSupply + currentItem.amount > dropItems[currentItem.itemId].maxSupply){
+            if (
+                dropItems[currentItem.itemId].currentSupply + currentItem.amount
+                    > dropItems[currentItem.itemId].maxSupply
+            ) {
                 revert CannotPurchaseItem(currentItem.itemId, currentItem.amount);
             }
-
+            // Ensure key is valid for use in current generation
             address keyContract = IHashFitFactory(FACTORY).fetchKeyByGen(keys[i].keyGen);
+            uint keyId = keys[i].keyId;
             IHashFitKey key = IHashFitKey(keyContract);
-            if (GENERATION - key.generation() < KEY_VALIDITY){
-                revert ExpiredKey(keys[i].keyId);
+            
+            if (GENERATION - key.generation() < key.keyValidity()) {
+                revert ExpiredKey(keyId);
             }
-
-            if (IERC721A(keyContract).balanceOf(msg.sender) < 1){
+            // Assert key ownership
+            if (IERC721A(keyContract).balanceOf(msg.sender) < 1) {
                 revert InsufficientKeys(keyContract);
             }
-            if (msg.sender != IERC721A(keyContract).ownerOf(keys[i].keyId)){
-                revert UnauthorizedKeyUsage(keys[i].keyGen, keys[i].keyId);
+
+            if (msg.sender != IERC721A(keyContract).ownerOf(keyId)) {
+                revert UnauthorizedKeyUsage(keys[i].keyGen, keyId);
             }
-            try IERC721A(keyContract).safeTransferFrom(msg.sender, IHashFitFactory(FACTORY).keyBurner(), keys[i].keyId){
-               emit RedeemKey(msg.sender, key.generation(), keys[i].keyId);
-            }
-            catch{
+            // Burn key and mint, validate sale and mint identity SBT
+            bytes memory burnInstruction = abi.encode(keyContract, keyId);
+            try IERC721A(keyContract)
+                .safeTransferFrom(msg.sender, IHashFitFactory(FACTORY).keyBurner(), keyId, burnInstruction) {
+                emit RedeemKey(msg.sender, key.generation(), keyId);
+            } catch {
                 revert UnableToTransferKey();
             }
 
@@ -140,42 +164,70 @@ contract HashFit is ERC1155{
             emit PurchaseAndClaim(currentItem.itemId, currentItem.amount, true);
             _mint(msg.sender, currentItem.amount, currentItem.itemId, "");
         }
-
     }
 
     function uri(uint256 itemId) public view override returns (string memory) {
-        if (!_itemExists(itemId)){
+        if (!_itemExists(itemId)) {
             revert UriRequestForNonExistentToken();
         }
         return dropItems[itemId].uri;
     }
 
     /// @dev Number of units of an item remaining in the drop
-    function itemsLeft(uint itemId) external view returns(uint){
+    function itemsLeft(uint256 itemId) external view returns (uint256) {
         return dropItems[itemId].maxSupply - dropItems[itemId].currentSupply;
     }
 
-    function _itemExists(uint itemId) internal view returns(bool){
+    function _itemExists(uint256 itemId) internal view returns (bool) {
         return dropItems[itemId].maxSupply != 0;
-    } 
-
+    }
 
     // Prevent Transferablity
-    function safeTransferFrom(address /*from*/, address /*to*/, uint256 /*tokenId*/, uint256 /*value*/, bytes memory /*data*/) public pure override{
+    function safeTransferFrom(
+        address,
+        /*from*/
+        address,
+        /*to*/
+        uint256,
+        /*tokenId*/
+        uint256,
+        /*value*/
+        bytes memory /*data*/
+    )
+        public
+        pure
+        override
+    {
         revert NonTransferrable();
     }
 
     function safeBatchTransferFrom(
-        address /*from*/,
-        address /*to*/,
-        uint256[] memory /*ids*/,
-        uint256[] memory /*values*/,
+        address,
+        /*from*/
+        address,
+        /*to*/
+        uint256[] memory,
+        /*ids*/
+        uint256[] memory,
+        /*values*/
         bytes memory /*data*/
-    ) public pure override{
+    )
+        public
+        pure
+        override
+    {
         revert NonTransferrable();
     }
-    
-    function setApprovalForAll(address /*operator*/, bool /*approved*/) public pure override{
+
+    function setApprovalForAll(
+        address,
+        /*operator*/
+        bool /*approved*/
+    )
+        public
+        pure
+        override
+    {
         revert NonTransferrable();
     }
 }
