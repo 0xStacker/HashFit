9652865d7dffa31086d50f0fbf2646eed59312f1
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-01-01 18:40:40 +0100

    update + document

--- src/IHashFitKey.sol
+++ src/IHashFitKey.sol
@@ -2,17 +2,21 @@
 
 pragma solidity ^0.8.25;
 
-interface IHashFitKey{
-
-    event DistributeKeys(address indexed receiver, uint amount);
+interface IHashFitKey {
+    event DistributeKeys(address indexed receiver, uint256 amount);
     error UnauthorizedAccess();
+    error MythicKeyNonTransferrable();
 
-    struct Receiver{
+    struct Receiver {
         address receiverAddress;
-        uint amount;
+        uint256 amount;
     }
-    
+
     function distributeKeys(Receiver[] memory receivers) external;
 
     function generation() external returns(uint);
+
+    function keyValidity() external view returns(uint);
+
+    function destroyKey(uint keyId) external;
 }
