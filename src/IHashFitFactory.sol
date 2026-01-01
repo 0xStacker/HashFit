8b7ef8259b7326ab371c16edddd45dacf1a19618
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-01-01 18:41:02 +0100

    update

--- src/IHashFitFactory.sol
+++ src/IHashFitFactory.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-interface IHashFitFactory{
-    function fetchKeyByGen(uint keyGen) external returns(address);
-    function keyBurner() external returns(address);
+interface IHashFitFactory {
+    function fetchKeyByGen(uint256 keyGen) external returns (address);
+    function keyBurner() external returns (address);
 
     event NewBurnerSet(address newBurner);
 }
