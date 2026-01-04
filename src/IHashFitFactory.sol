9ce15edef6476f2b768f3562a6ad88fe7950ffa3
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-01-04 07:06:03 +0100

    update factory interface

--- src/IHashFitFactory.sol
+++ src/IHashFitFactory.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.25;
 
 interface IHashFitFactory {
-    function fetchKeyByGen(uint256 keyGen) external returns (address);
+    function fetchKeyByGen(uint256 keyGen) external returns (address, address);
     function keyBurner() external returns (address);
 
     event NewBurnerSet(address newBurner);
