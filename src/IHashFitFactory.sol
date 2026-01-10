5fd588cac674de6a9072d231a05b2679a8c4d1c4
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-01-10 07:36:37 +0100

    make fetchKeyBy gen return only legendary keys

--- src/IHashFitFactory.sol
+++ src/IHashFitFactory.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.25;
 
 interface IHashFitFactory {
-    function fetchKeyByGen(uint256 keyGen) external returns (address, address);
+    function fetchKeyByGen(uint256 keyGen) external returns (address);
     function keyBurner() external returns (address);
+    function mythic() external returns(address);
+    function epic() external returns(address);
 
     event NewBurnerSet(address newBurner);
 }
