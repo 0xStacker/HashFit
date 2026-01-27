cb32876b4b6cf00ceaaf96047db6d9a4f328c42f
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-01-28 00:14:09 +0100

    refactor

--- src/interfaces/IHashFitFactory.sol
+++ src/interfaces/IHashFitFactory.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
-import {HashFitMythic, HashFitEpic} from "../HashFitKeys.sol";
+import {HashFitMythic, HashFitLegendary, HashFitEpic} from "../HashFitKeys.sol";
 
 interface IHashFitFactory {
-    function fetchKeyByGen(uint256 keyGen) external returns (address);
+    function fetchKeyByGen(uint256 keyGen) external returns (HashFitLegendary);
     function keyBurner() external returns (address);
-    function mythic() external returns(HashFitEpic);
+    function mythic() external returns(HashFitMythic);
+    function epic() external returns(HashFitEpic);
 
     event NewBurnerSet(address newBurner);
-    event RestockItem(uint, uint, uint);
-    event SetDiscount(uint64, uint8, uint64);
+  
 }
