1ac734e521a96a45a10b05c1eb1c9880a687a352
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-02-01 21:22:35 +0100

    add docs

--- src/HashFitAdmin.sol
+++ src/HashFitAdmin.sol
@@ -6,6 +6,15 @@ import {HashFitTypes} from "./Types.sol";
 import {HashFitMythic, HashFitLegendary, HashFitEpic} from "./HashFitKeys.sol";
 import {HashFitCore} from "./HashFit.sol";
 
+/**
+ * @title HashFit Admin management contract
+ * @author Ibrahim🐸
+ * 
+ * This contract is resposible for handling and managing all administrative 
+ * functionalities across all HashFit contracts
+ * It deploys and manage HashFit Factory and every drop contracts spawned by the Factory
+ */
+
 contract HashFitAdmin{
     /// @dev Factory controlled by admin contract
     HashFitFactory public immutable FACTORY;
@@ -15,14 +24,18 @@ contract HashFitAdmin{
         FACTORY = new HashFitFactory(_epic, _mythic);
     }
 
+    // Emitted when admin restocks a particular item within a drop gen
     event Command__RestockItem(uint64 _gen, uint8 _itemId, uint _restockAmount);
+    // Emitted when admin applies discount to a certain item within a drop gen
     event Command__SetDiscount(uint64 _gen, uint8 itemId, uint64 discount);
+    // Emitted when admin changes the contracts that burns redeemed keys
     event Command__SetKeyBurner(address _newBurner);
+    // Emitted when admins distributes keys
     event Command__DistributeKeys(HashFitTypes.KeyTier indexed tier);
 
     error InvalidKeyType();
 
-    /// @dev Restocks an item in a particular drop generation.abi
+    /// @dev Admin function to restock an item in a particular drop generation
     /// @param gen is the drop generation for which the item was created
     /// @param itemId is the identifier for the item within the drop gen
     /// @param restockAmount is the amount of that item to be restocked
@@ -31,27 +44,38 @@ contract HashFitAdmin{
         drop.restock(itemId, restockAmount);
         emit Command__RestockItem(gen, itemId, restockAmount);
     }
-
+    
+    /// @dev Admin function to add discount to a unique item within a drop gen
+    /// @param gen is the drop generation which the item belongs
+    /// @param itemId is the unique identifier of the item within the drop gen
+    /// @param discountBPS is the percentage discount to be applied to the item (100bps = 1%)
     function setDiscount(uint64 gen, uint8 itemId, uint64 discountBps) external {
         HashFitCore drop = HashFitCore(FACTORY.drop(gen));
         emit Command__SetDiscount();
     }
 
+    /// @dev Admin function to set new burner address for keys
+    /// @param _newBurner is the new address which keys would be sent to for incilneration
     function setKeyBurner(address _newBurner) external {
         FACTORY.setKeyBurner(_newBurner);
         emit Command__SetKeyBurner(_newBurner);
     }
-
-    function distributeLegendary(HashFitTypes.Receiver[] memory _receivers, HashFitTypes.KeyTier tier, uint gen) external{
-        // reject invalid key types
-        if (tier != HashFitTypes.KeyTier.LEGENDARY){
-            revert InvalidKeyType();
-        }
+    
+    /// @dev Admin function to distribute legendary keys for a particular gen
+    /// @notice keys can only be distributed for a gen once
+    /// @param _receivers contain the addresses of the winners, computed off chain
+    /// @param gen is the key generation to be distributed
+    function distributeLegendary(HashFitTypes.Receiver[] memory _receivers, uint gen) external{
         HashFitLegendary legendary = FACTORY.fetchKeyByGen(gen);
         legendary.distributeKeys(_receivers);
         emit Command__DistributeKeys(tier);
     }
 
+    /// @dev Admin function to distribute mythic and epic keys]
+    /// @param _receivers contains the addresses of the winners to which keys would be distributed.
+    /// @param tier is the key tier to distribute
+    /// - Mythic
+    /// - Epic 
     function distributeOthers(HashFitTypes.Receiver[] memory _receivers, HashFitTypes.KeyTier tier) external{
         // reject invalid key types
         if(tier != HashFitTypes.KeyTier.MYTHIC && tier != HashFitTypes.KeyTier.EPIC){
