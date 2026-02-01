053a1c7489f14676d70c3d1767b679850e122dc8
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-02-01 21:24:19 +0100

    add docs

--- src/interfaces/IHashFitFactory.sol
+++ src/interfaces/IHashFitFactory.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.8.25;
 import {HashFitMythic, HashFitLegendary, HashFitEpic} from "../HashFitKeys.sol";
 
 interface IHashFitFactory {
-    function fetchKeyByGen(uint256 keyGen) external returns (HashFitLegendary);
-    function keyBurner() external returns (address);
-    function mythic() external returns(HashFitMythic);
-    function epic() external returns(HashFitEpic);
+    //Fetches the Legendary key contract for a particular drop gen
+    // gen is the drop generation to fetch key for
+    function fetchKeyByGen(uint256 gen) external returns (HashFitLegendary);
 
+    // Fetches the address of the key burner
+    // The burner is responsible for destroying redeemed keys
+    function keyBurner() external returns (address);
+
+    // Fetches the non changing mythic key 
+    function mythic() external returns(HashFitMythic);
+    // Fetches the epic key
+    function epic() external returns(HashFitEpic);
     event NewBurnerSet(address newBurner);
   
 }
