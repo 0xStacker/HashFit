d17cd60461b53edc07f500bc49d41448c914ed65
0xstacker <oladotuntemitopeakorede@gmail.com>
2026-02-06 22:32:37 +0100

    refactor

--- src/HashFitExclusive.sol
+++ src/HashFitExclusive.sol
@@ -8,24 +8,37 @@ import {IHashFitKey} from "./interfaces/IHashFitKey.sol";
 import {HashFitMythic} from "./HashFitKeys.sol";
 import {IERC721A} from "@ERC721A/IERC721A.sol";
 
+/**
+ * @title HashFit Exclusive Drop
+ * @author Ibrahim🐸
+ * This is an exclusive version of HashFitCore.
+ * Only whitelisted addresses are allowed to purchase items from the drop
+ */
+
 contract HashFitExclusive is HashFitCore {
     using MerkleProof for bytes32[];
+    // Merkle root for addresses allowed to purchase an item from the drop
+    bytes32 immutable ROOT;
 
-    bytes32 immutable root;
-
-    constructor(string memory _uri, bytes32 merkleRoot, HashFitTypes.HashFitDrop memory setup, address _admin) HashFitCore(_uri, setup, _admin) {
+    // Initialize contract with necessary data
+    constructor(string memory _uri, bytes32 merkleRoot, HashFitTypes.HashFitDrop memory setup, address _admin)
+        HashFitCore(_uri, setup, _admin)
+    {
         root = merkleRoot;
     }
 
-
+    // Thrown when a non whitelisted user attempts a purchase
     error NotWhiteListed();
+    // Enforce whitelist priviledges
     modifier onlyWhitelist(bytes32[] memory proof) {
-        if (!proof.verify(root, keccak256(abi.encode(msg.sender)))) {
+        if (!proof.verify(ROOT, keccak256(abi.encode(msg.sender)))) {
             revert NotWhiteListed();
         }
         _;
     }
 
+    /// @inheritdoc HashFitCore
+    /// @param proof is the merkle proof used to validate user
     function purchaseAndClaim(HashFitTypes.SaleItem[] memory _items, bytes32[] memory proof)
         external
         payable
@@ -35,6 +48,7 @@ contract HashFitExclusive is HashFitCore {
         _purchaseAndClaim(_items);
     }
 
+    /// @inheritdoc HashFitCore
     function purchaseWithKey(HashFitTypes.SaleItem[] memory _items, KeyInfo[] memory keys) external override {
         // Sanity check
         if (_items.length != keys.length) {
@@ -52,17 +66,14 @@ contract HashFitExclusive is HashFitCore {
         for (uint256 i; i < _items.length; i++) {
             // Make sure item is not sold out
             HashFitTypes.SaleItem memory currentItem = _items[i];
-            if (
-                currentSupply[currentItem.itemId] + currentItem.amount
-                    > dropItems[currentItem.itemId].maxSupply
-            ) {
+            if (currentSupply[currentItem.itemId] + currentItem.amount > dropItems[currentItem.itemId].maxSupply) {
                 revert CannotPurchaseItem(currentItem.itemId, currentItem.amount);
             }
             // Use the corresponding key for current item
             uint256 keyId = keys[i].keyId;
 
             if (msg.sender != IERC721A(keyContract).ownerOf(keyId)) {
-                revert UnauthorizedKeyUsage(keys[i].keyGen, keyId);
+                revert UnauthorizedKeyUsage(keys[i].gen, keyId);
             }
             // Burn key and mint, validate sale and mint identity SBT
             bytes memory burnInstruction = abi.encode(keyContract, keyId);
