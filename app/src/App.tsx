21866e02df5ced5157fc2fad26143366d93e5b23
@0xStacker <oladotuntemitopeakorede@gmail.com>
2026-03-04 17:25:11 +0100

    update frontend

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,12 @@
 import { Home } from "./HomePage";
-import "./App.css";
-import { NavBar } from "./components/NavBar";
+import { Shop } from "./Shop";
+import { Routes, Route } from "react-router-dom";
 
 export function App() {
   return (
-    <>
-      <title>HashFit. Do More</title>
-      <div className="app">
-        <Home />
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop/drop" element={<Shop />} />
+    </Routes>
   );
 }
